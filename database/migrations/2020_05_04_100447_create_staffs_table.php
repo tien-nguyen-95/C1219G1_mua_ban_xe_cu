19cb7fef9de23a380866ba86189d9c81b1c07717
Admin <you@example.com>
2020-05-04 17:33:43 +0700

    thuat-migration-1

---
+++ database/migrations/2020_05_04_100447_create_staffs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->unique();
+            $table->tinyInteger('gender');
+            $table->date('birthday');
+            $table->string('phone', );
+            $table->longText('image');
+            $table->integer('account_id');
+            $table->integer('position_id');
+            $table->string('address', 255);
+            $table->integer('branch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+}
