1e0e53e37412b78c9d770b126e370ebc170c7071
hoangnguyen294 <58716090+hoangnguyen294@users.noreply.github.com>
2020-05-05 09:41:26 +0700

    hoang-seed

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tag;
+use Faker\Generator as Faker;
+use phpDocumentor\Reflection\Types\Nullable;
+
+$factory->define(Tag::class, function (Faker $faker) {
+    return [
+        'title' => $faker->firstName,
+        'category_id'=>$faker->randomDigitNotNull,
+    ];
+});
