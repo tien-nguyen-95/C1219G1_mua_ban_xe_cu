025406be67469af49fb009546f8a287bdd23b8f5
Admin <you@example.com>
2020-05-14 22:54:29 +0700

    thuat crud, show detail customer, còn thiếu thùng rác và khôi phục

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -5,5 +5,5 @@ interface CustomerRepository extends Repository
 {
     public function getTrash();
     public function restore($id);
-    public function hardDelete($id);
+    public function delete($id);
 }
