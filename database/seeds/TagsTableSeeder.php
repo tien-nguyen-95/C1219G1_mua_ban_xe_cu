5929b7acc8bfbaa386e3f79c9c8cfc61707a2fca
tien-nguyen-95 <quangtiennd95@gmail.com>
2020-05-31 23:01:24 +0700

    tien-fix-seeder: git commit -mtien-fix-seeder git commit -mtien-fix-seeder:

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -12,48 +12,55 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        $tag = new Tag();
-        $tag->title = 'tag';
-        $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag1';
+        $tag->category_id = 1;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag2';
+        $tag->category_id = 2;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag3';
+        $tag->category_id = 2;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag4';
+        $tag->category_id = 3;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag5';
+        $tag->category_id = 3;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag6';
+        $tag->category_id = 4;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag7';
+        $tag->category_id = 4;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag8';
+        $tag->category_id = 5;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag9';
+        $tag->category_id = 5;
         $tag->save();
 
         $tag = new Tag();
         $tag->title = 'tag10';
+        $tag->category_id = 1;
         $tag->save();
     }
 }
